00ac48d529c074b8ba7affbd92c61ac1c92e5baa
Vitor <vitorvitinho9900@hotmail.com>
2024-03-19 11:29:55 -0300

    inicio trabalho 2

--- Trabalho 2/trab.py
+++ Trabalho 2/trab.py
@@ -1,19 +1,20 @@
-def ler_dados(arquivo):
-    dados = {
-        "data_1": {
-            "peso": [382745, 799601, 909247, 729069, 467902, 44328, 34610, 698150, 823460, 903959, 853665, 551830, 610856, 670702, 488960, 951111, 323046, 446298, 931161, 31385, 496951, 264724, 224916, 169684],
-            "utilidade": [825594, 1677009, 1676628, 1523970, 943972, 97426, 69666, 1296457, 1679693, 1902996, 1844992, 1049289, 1252836, 1319836, 953277, 2067538, 675367, 853655, 1826027, 65731, 901489, 577243, 466257, 369261],
-        },
-        "data_2": {
-            "peso": [23, 31, 29, 44, 53, 38, 63, 85, 89, 82],
-            "utilidade": [92, 57, 49, 68, 60, 43, 67, 84, 87, 72],
-        }
-    }
+def ler_dados(caminho_arquivo):
+    with open(caminho_arquivo, 'r') as arquivo:
+        linhas = arquivo.read().splitlines()
+        indice_n = next(i for i, linha in enumerate(linhas) if linha.startswith('n ='))
+        
+        linhas_pesos = ' '.join(linha.split('peso: ')[-1] for linha in linhas[:indice_n] if linha.startswith('peso:'))
+        linhas_utilidades = ' '.join(linha.split('utilidade: ')[-1] for linha in linhas[:indice_n] if linha.startswith('utilidade:'))
+        
+        pesos = list(map(int, linhas_pesos.split()))
+        utilidades = list(map(int, linhas_utilidades.split()))
 
-    return dados[arquivo]["peso"], dados[arquivo]["utilidade"]
+        n = int(linhas[indice_n].split('n = ')[1])
 
-def borsa(arquivo, n):
-    peso, utilidade = ler_dados(arquivo)
+    return pesos, utilidades, n
+
+def borsa(caminho_arquivo):
+    peso, utilidade, n = ler_dados(caminho_arquivo)
     itens = list(range(len(peso)))
     densidade_valor = [(utilidade[i] / peso[i], i) for i in itens]
     
@@ -31,14 +32,13 @@ def borsa(arquivo, n):
 
     return itens_selecionados, peso_total, utilidade_total
 
-itens_selecionados_data_1, peso_total_data_1, utilidade_total_data_1 = borsa("data_1", 6404180)
+itens_selecionados_data_1, peso_total_data_1, utilidade_total_data_1 = borsa("data_1.txt")
 print("Data 1:")
 print("Itens selecionados:", itens_selecionados_data_1)
 print("Peso total:", peso_total_data_1)
 print("Utilidade total:", utilidade_total_data_1)
-print()
 
-itens_selecionados_data_2, peso_total_data_2, utilidade_total_data_2 = borsa("data_2", 165)
+itens_selecionados_data_2, peso_total_data_2, utilidade_total_data_2 = borsa("data_2.txt")
 print("Data 2:")
 print("Itens selecionados:", itens_selecionados_data_2)
 print("Peso total:", peso_total_data_2)
